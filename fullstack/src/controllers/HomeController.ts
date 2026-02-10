77aafb7c0a4116dadb5bac83e1901f53e2d87b87
gabriela611 <gsanabriag@eafit.edu.co>
2026-02-09 20:56:54 -0500

    Tutorial #2

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,38 +1,36 @@
 import type { Request, Response } from 'express';
-
+import { books } from '../data/books.js';
+import { Book } from '../models/Book.js';
 
 export class HomeController {
 
 static index(req: Request, res: Response): void {
-const viewData: { [key: string]: any } = {};
-
-viewData["title"] = "Home";
-
-res.render('home/index', { viewData: viewData });
-
-
+    const viewData: { [key: string]: any } = {};
+    viewData["title"] = "Home";
+    res.render('home/index', { viewData: viewData });
 }
 
 static about(req: Request, res: Response): void {
-
-const viewData: { [key: string]: any } = {};
-
-viewData["title"] = "About";
-
-
-res.render('home/about', { viewData: viewData });
-
+    const viewData: { [key: string]: any } = {};
+    viewData["title"] = "About";
+    res.render('home/about', { viewData: viewData });
 }
 
 static contact(req: Request, res: Response): void {
+    const viewData: { [key: string]: any } = {};
+    viewData["title"] = "Contact";
+    res.render('home/contact', { viewData: viewData });
+}
 
-const viewData: { [key: string]: any } = {};
+static Main_Point(req: Request, res: any) {
+    const viewData: any = {};
+    viewData["books"] = books;
+    res.render('home/books', viewData);
+}
 
-viewData["title"] = "Contact";
-
-
-res.render('home/contact', { viewData: viewData });
-
+static show(req: any, res: any){
+    const book = Book.findById(books, parseInt(req.params.id));
+    res.render('home/show', { book: book })
 }
 
 }
--- fullstack/src/routes/Routes.ts
+++ fullstack/src/routes/Routes.ts
@@ -2,17 +2,17 @@ import { Router } from 'express';
 
 import { HomeController } from '../controllers/HomeController.js';
 
-
 export default class Routes {
 
 static initializeRoutes(): Router {
 
 const router = Router();
 
-
 router.get('/', HomeController.index);
 router.get('/about', HomeController.about);
 router.get('/contact', HomeController.contact);
+router.get('/main-point', HomeController.Main_Point);
+router.get('/books/:id', HomeController.show);
 return router;
 
 }
