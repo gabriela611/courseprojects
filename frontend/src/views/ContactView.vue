07854443aa1d5e0fcdfb18842aff471ff3cd087b
gabriela611 <gsanabriag@eafit.edu.co>
2026-02-10 17:35:42 -0500

    Tutorial #3 completed w/ activity

---
+++ frontend/src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="mt-4 mb-4">
+    <div class="max-w-7xl mx-auto">
+      <h2 class="text-3xl font-bold text-gray-800 text-center">Contact Page</h2>
+    </div>
+    <div class="max-w-7xl mx-auto">
+      <h3 class="text-1xl font-bold text-pink-800 text-center">You can find me at any Frisby location ! :3 .</h3>
+    </div>
+  </section>
+</template>
